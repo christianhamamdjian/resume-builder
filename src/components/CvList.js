aeae2ab8eb3ab483f532db04cb9ef79d6a316c48
Christian Hamamdjian <christian.hamamdjian@welho.com>
2023-12-25 03:42:01 +0200

    Fixing logout

--- src/components/CvList.js
+++ src/components/CvList.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 //import { Link } from 'react-router-dom'
 import CreateCv from './CreateCv'
+import AuthContext from "../context/authContext";
 import { BuilderContext } from './../App'
 
 const CvList = () => {
 	const ctx = useContext(BuilderContext)
-
+	const { user } = useContext(AuthContext)
 	const renderCvs = () => ctx.infoState.map((cv, i) => {
 		const { data } = cv
 		const { ref } = cv
@@ -24,13 +25,17 @@ const CvList = () => {
 
 	return (
 		<>
-			<div className="flex px-4 justify-between">
-				<h1 className="flex-none w-34 h-8"><strong>Your CVs:</strong> </h1>
-				<CreateCv />
-			</div>
-			<ul className="h-36 border-2 border-grey border-solid p-2 overflow-y-auto">
-				{renderCvs()}
-			</ul>
+			{user && (
+				<>
+					<div className="flex px-4 justify-between">
+						<h1 className="flex-none w-34 h-8"><strong>Your CVs:</strong> </h1>
+						<CreateCv />
+					</div>
+					<ul className="h-36 border-2 border-grey border-solid p-2 overflow-y-auto">
+						{renderCvs()}
+					</ul>
+				</>
+			)}
 		</>
 	);
 };
